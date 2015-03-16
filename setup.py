6825f8f625e8bf5eef2433085809014b989ba3cd
Stuart Axon <stuaxo2@yahoo.com>
2015-03-16 22:34:54 +0700

    Merge

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ class fixup_install_data(install_data):
 setup(
     cmdclass={'install_data': fixup_install_data },
     name='vext',
-    version='0.2.10',
+    version='0.2.11',
 
     description='Use system python packages in a virtualenv',
     long_description=long_description,
