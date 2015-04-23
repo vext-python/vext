def04ba891b99cc5217c3eb662b58811d5ffb479
Stuart Axon <stuaxo2@yahoo.com>
2015-04-24 01:29:26 +0700

    Add 'clean'

--- setup.py
+++ setup.py
@@ -11,12 +11,14 @@ if set(["install", "develop", "build", "sdist"]).intersection(sys.argv):
 
 
 from glob import glob
-from os.path import abspath, basename, dirname, join, normpath
+from os.path import abspath, basename, dirname, join, normpath, relpath
+from shutil import rmtree
 
 from distutils import sysconfig
 from distutils.command.build import build
 
 from setuptools import setup
+from setuptools import Command
 from setuptools.command.develop import develop
 from setuptools.command.easy_install import easy_install
 
@@ -48,6 +50,29 @@ class DevelopWithPTH(develop):
         dest = join(self.install_dir, basename(path))
         self.copy_file(path, dest)
 
+class CleanCommand(Command):
+    """Custom clean command to tidy up the project root."""
+    CLEAN_FILES = './build ./dist ./*.pyc ./*.tgz ./*.egg-info'.split(' ')
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        global here
+
+        for path_spec in self.CLEAN_FILES:
+            # Make paths absolute and relative to this path
+            abs_paths = glob(normpath(join(here, path_spec)))
+            for path in [str(p) for p in abs_paths]:
+                if not path.startswith(here):
+                    # Die if path in CLEAN_FILES is absolute + outside this directory
+                    raise ValueError("%s is not a path inside %s" % (path, here))
+                print('removing %s' % relpath(path))
+                rmtree(path)
+
 long_description=open('DESCRIPTION.rst').read()
 
 setup(
@@ -55,6 +80,7 @@ setup(
         'build': BuildWithPTH,
         'easy_install': EasyInstallWithPTH,
         'develop': DevelopWithPTH,
+        'clean': CleanCommand,
     },
 
     name='vext',
