4a1cb9b6ac756157193ceae613430f0cc411851f
Stuart Axon <stu.axon@gmail.com>
2015-12-02 02:19:20 +0800

    Moved some things.

--- tests/tests.py
+++ tests/tests.py
@@ -1,12 +1,15 @@
 import vext
 
 # Create a temporary virtualenv and run these
+import vext.env
+
+
 def test_findsyspy():
-    vext.findsyspy()
+    vext.env.findsyspy()
 
 
 def test_invenv():
-    print(vext.in_venv())
+    print(vext.env.in_venv())
 
 test_findsyspy()
 test_invenv()
