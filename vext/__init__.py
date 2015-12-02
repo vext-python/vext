cd1f7734149fccc4c3760bb56babf03b4029ed4a
Stuart Axon <stu.axon@gmail.com>
2015-12-02 17:56:54 +0800

    Typo :/

--- vext/__init__.py
+++ vext/__init__.py
@@ -9,7 +9,7 @@ if "VEXT_DEBUG_LOG" in environ:
 
 vext_pth = join(get_python_lib(), 'vext_importer.pth')
 
-llogger = logging.getLogger("vext")
+logger = logging.getLogger("vext")
 
 def install_importer():
     logger.debug("install_importer has been moved to gatekeeper module")
