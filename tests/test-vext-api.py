4c3cd6f8dd8b061cb1e78a2e2eb26515cc33a213
Stuart Axon <stu.axon@gmail.com>
2016-04-28 22:52:48 +0100

    .travis.yml
    Test stubs - at least we will know if things are broken.

--- tests/test-vext-api.py
+++ tests/test-vext-api.py
@@ -8,10 +8,10 @@ Basically stubs right now.
 
 
 class TestVextAPI(unittest.TestCase):
-    def test_findsyspy():
+    def test_findsyspy(self):
         vext.env.findsyspy()
 
-    def test_invenv():
+    def test_invenv(self):
         vext.env.in_venv()
 
 if __name__ == '__main__':
