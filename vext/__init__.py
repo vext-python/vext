3ff1d3edbdbbdce537802a2a51a4cbe197928b92
Stuart Axon <stuaxo2@yahoo.com>
2017-11-21 01:26:47 +0000

    Update __init__.py
    Don't call basicConfig in logger.

--- vext/__init__.py
+++ vext/__init__.py
@@ -4,15 +4,17 @@ from os import environ
 from os.path import join
 from distutils.sysconfig import get_python_lib
 
-if environ.get("VEXT_DEBUG_LOG", "0") == "1":
-    logging.basicConfig(level=logging.DEBUG)
-else:
-    logging.basicConfig(level=logging.ERROR)
+VEXT_DEBUG_LOG = "VEXT_DEBUG_LOG"
     
 vext_pth = join(get_python_lib(), 'vext_importer.pth')
 
 logger = logging.getLogger("vext")
-
+if VEXT_DEBUG_LOG in environ:
+    if environ.get(VEXT_DEBUG_LOG, "0") == "1":
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.addHandler(logging.NullHandler())
+    
 def install_importer():
     logger.debug("install_importer has been moved to gatekeeper module")
     from vext import gatekeeper
