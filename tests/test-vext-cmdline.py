3d83904e409eecfd44b0c0ca053f78da5c9c89a4
Stuart Axon <stu.axon@gmail.com>
2020-08-06 11:49:13 +0100

    Add stub test for do_status

--- tests/test-vext-cmdline.py
+++ tests/test-vext-cmdline.py
@@ -1,12 +1,18 @@
 import unittest
 
-from vext.cmdline import do_check
+from vext.cmdline import do_check, do_status
 
 
 class TestVextCommandLineHelpers(unittest.TestCase):
     def test_do_check(self):
         # Stub check: verifies no exceptions are thrown.
         do_check(["*"])
+
+    def test_do_status(self):
+        # Stub check: verifies no exceptions are thrown.
+
+        # TODO, trigger different statuses and check messages printed.
+        do_status()
 
 
 if __name__ == "__main__":
