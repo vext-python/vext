993bb90ac98733579a1d0e4ac9cd05528dd0a278
Stuart Axon <stu.axon@gmail.com>
2020-08-12 16:35:03 +0100

    Change script directory handling.

--- scripts/test_virtualenv.sh
+++ scripts/test_virtualenv.sh
@@ -3,8 +3,9 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
 ENV=/tmp/vext-test-virtualenv
 
 rm -fR "${ENV}"
 virtualenv -q -p $(which python3) "${ENV}"
-source _test-env.sh "${ENV}"
+source $SCRIPT_DIR/_test-env.sh "${ENV}"
