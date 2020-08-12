c349cf269c04e7df601d0d378db1bffa1739299f
Stuart Axon <stu.axon@gmail.com>
2020-08-12 17:06:47 +0100

    Try and pass correct python version.

--- scripts/test_virtualenv.sh
+++ scripts/test_virtualenv.sh
@@ -7,5 +7,7 @@ SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
 ENV=/tmp/vext-test-virtualenv
 
 rm -fR "${ENV}"
-virtualenv -q -p $(which python3) "${ENV}"
+echo virtualenv -q -p $(which python) "${ENV}"
+virtualenv -q -p $(which python) "${ENV}"
+echo source $SCRIPT_DIR/_test-env.sh "${ENV}"
 source $SCRIPT_DIR/_test-env.sh "${ENV}"
